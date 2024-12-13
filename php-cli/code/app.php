aab9b8c7a73d36a7165edb74b9d337274fcc4a15
Svetlana <medjog@gmail.com>
2024-12-13 19:05:08 +0400

    add commands for project

--- php-cli/code/app.php
+++ php-cli/code/app.php
@@ -6,3 +6,13 @@ require_once('vendor/autoload.php');
 $result = main("/code/config.ini");
 // вывод результата
 echo $result; 
+
+// команды в проекте
+// composer install
+// docker build -t myapp/php-cli ./php-cli
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php help
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php add
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php del
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php clear
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php read-profiles
+// docker run -it -v ${pwd}/php-cli/code:/code myapp/php-cli php /code/app.php read-profile 
